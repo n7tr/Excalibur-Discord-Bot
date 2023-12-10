b1f618c62b93f7b6acb5254c1027f68411af77c1
N77R <100294280+n7tr@users.noreply.github.com>
2023-12-10 16:38:29 +0300

    Create main.go

---
+++ src/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	godotenv.Load()
+	BOT_TOKEN := os.Getenv("BOT_TOKEN")
+	sess, err := discordgo.New("Bot " + BOT_TOKEN)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Handlers
+
+	sess.AddHandler(onGuildCreate)
+
+	//Intents
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
+
+	//Authorization
+	err = sess.Open()
+
+	//Set Status
+	t := time.Now()
+	elapsed := time.Since(t)
+	sess.UpdateStreamingStatus(0, "De Inferno ["+fmt.Sprint(elapsed.Nanoseconds())+" ms]", "https://www.twitch.tv/404")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sess.Close()
+
+	fmt.Println("The bot is online!\nTOKEN: " + BOT_TOKEN)
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+}
