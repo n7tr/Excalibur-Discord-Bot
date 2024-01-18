47125cda5696793d65594c4aa7847f5b80afb4cd
N77R <100294280+n7tr@users.noreply.github.com>
2024-01-18 15:37:12 +0300

    Create sendhttp.go

---
+++ Inferno/cogs/sendhttp.go
@@ -0,0 +1,37 @@
+package cogs
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func Sendhttp(url string, method string, data []byte) string {
+	godotenv.Load()
+	TOKEN := os.Getenv("BOT_TOKEN")
+	client := &http.Client{}
+
+	var req *http.Request
+
+	if data != nil {
+		req, _ = http.NewRequest(method, url, bytes.NewBuffer(data))
+	} else {
+		req, _ = http.NewRequest(method, url, nil)
+	}
+
+	req.Header.Set("Authorization", "Bot "+TOKEN)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+
+	data, _ = io.ReadAll(resp.Body)
+
+	return string(data)
+}
